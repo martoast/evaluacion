f1c38b06ed939875a78ab8b8845e7bab49996621
Alejandro Martos <alexmartos96@gmail.com>
2022-06-13 15:16:35 -0700

    card components

---
+++ components/cards/MonthlyPayment.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-card class="border-05 border-0 shadow mt-4" no-body>
+      <b-card-body class="p-4">
+        <b-card-sub-title
+          sub-title-text-variant="primary"
+          class="font-weight-bold mb-3"
+        >
+          <b-img src="/icons/monthly-payment.svg"></b-img> Home Equity Gain
+        </b-card-sub-title>
+        <div class="text-center">
+          <b-img src="/icons/slider.svg"></b-img>
+        </div>
+        <div
+          class="d-flex justify-content-center"
+          style="align-items: self-end"
+        >
+          <p class="text-light-gray">*Choose Loan Terms</p>
+          <b-dropdown variant="outline-danger" text="5 years" class="m-2">
+            <b-dropdown-item href="#">5 years</b-dropdown-item>
+            <b-dropdown-item href="#">10 years</b-dropdown-item>
+            <b-dropdown-item href="#">30 years</b-dropdown-item>
+          </b-dropdown>
+        </div>
+      </b-card-body>
+    </b-card>
+  </div>
+</template>
---
+++ components/cards/PriceRange.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <b-card class="border-05 border-0 shadow mt-4" no-body>
+      <b-card-body class="p-4">
+        <b-card-sub-title
+          sub-title-text-variant="primary"
+          class="font-weight-bold mb-3"
+        >
+          <b-img src="/icons/home-cost.svg"></b-img> Home Equity Gain
+        </b-card-sub-title>
+
+        <div class="text-center">
+          <b-img src="/icons/price-range.svg"></b-img>
+        </div>
+
+        <p class="text-light-gray">*USD - US Dollar</p>
+      </b-card-body>
+    </b-card>
+  </div>
+</template>

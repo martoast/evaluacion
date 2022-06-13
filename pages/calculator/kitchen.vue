73d9211413a77187e92ab2e5d9496510cdbcbaa5
Jonathan Zúñiga <jzuniga@gomultitaskr.com>
2022-06-13 16:19:01 -0700

    kitchen page wip

--- pages/calculator/kitchen.vue
+++ pages/calculator/kitchen.vue
@@ -1,7 +1,168 @@
-<template></template>
+<template>
+  <div class="kitchen">
+    <div class="container-fluid">
+      <div class="block-container block-container--1">
+        <div class="block-container block-container--2">
+          <div class="block-container block-container--3">
+            <div class="block">
+              <div class="block__right">
+                <div>
+                  <img
+                    class="logo-customizer"
+                    src="/images/kitchen/logo-customizer.svg"
+                    alt="logo customizer image"
+                  />
+
+                  <h1>
+                    <span class="d-md-block">Customize</span> your
+                    <strong>Kitchen</strong>
+                  </h1>
+
+                  <img
+                    class="kitchen-image"
+                    src="/images/kitchen/kitchen.svg"
+                    alt="Kitchen image"
+                  />
+
+                  <img
+                    class="line"
+                    src="/images/kitchen/line.svg"
+                    alt="line image"
+                  />
+
+                  <div class="d-flex justify-content-between links">
+                    <span>Back</span>
+                    <span>Customize</span>
+                  </div>
+                </div>
+              </div>
+              <div class="block__left">
+                <kitchens-grid></kitchens-grid>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer></footer>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
-export default {};
+import KitchensGrid from "@/components/kitchen/KitchensGrid.vue";
+
+export default {
+  components: {
+    KitchensGrid,
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.kitchen {
+  color: #fff;
+  font-family: "Poppins";
+  letter-spacing: 0.02em;
+
+  img {
+    display: block;
+    max-width: 100%;
+  }
+
+  .logo-customizer {
+    margin-bottom: 3.5625rem;
+  }
+
+  h1 {
+    font-weight: 400;
+    line-height: 3.3125rem;
+
+    strong {
+      font-weight: 600;
+    }
+  }
+
+  &-image {
+    margin: 3.5625rem 0;
+  }
+
+  .line {
+    margin-bottom: 1.625rem;
+  }
+
+  .links {
+    max-width: 20rem;
+  }
+
+  .block {
+    @media screen and (min-width: 76rem) {
+      border: 0.375rem solid #ffffff;
+      border-radius: 0.25rem;
+
+      display: flex;
+    }
+
+    &-container {
+      &--1 {
+        background-image: url(/images/purple-glass.png);
+        background-repeat: no-repeat;
+        background-size: cover;
+        border-radius: 0.5rem;
+        margin: 0 auto;
+
+        max-width: 76rem;
+
+        @media screen and (min-width: 76rem) {
+          padding: 4rem;
+        }
+      }
+
+      &--2 {
+        @media screen and (min-width: 76rem) {
+          background-image: url(/images/kitchen/hero.svg);
+          background-repeat: no-repeat;
+          background-size: calc(50% + 2rem) 100%;
+        }
+      }
+
+      &--3 {
+        @media screen and (min-width: 76rem) {
+          padding: 4rem 0 4rem 4rem;
+        }
+      }
+    }
+
+    &__right,
+    &__left {
+      @media screen and (min-width: 76rem) {
+        width: 50%;
+      }
+    }
+
+    &__right {
+      font-weight: 600;
+      font-size: 1.1765rem;
+      line-height: 133%;
+      margin: 1rem 0;
+      padding: 1rem;
+
+      @media screen and (min-width: 76rem) {
+        display: flex;
+        align-items: center;
+        order: 2;
+
+        padding: 0 6.25rem;
+      }
+    }
+
+    &__left {
+      background-color: #fff;
+
+      @media screen and (min-width: 76rem) {
+        order: 1;
+        overflow: hidden;
+      }
+    }
+  }
+}
+</style>

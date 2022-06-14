2aff448d2136197d3786239d1c6e78d63fdd3b1b
Teresa Wu Feng <twufeng@gomultitaskr.com>
2022-06-13 18:32:41 -0700

    navbar component and add main layout

---
+++ components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-navbar class="main-navbar">
+    <div class="nav-logo">
+      <b-avatar src="/images/multitaskr-avatar.png"></b-avatar>
+    </div>
+
+    <b-navbar-nav class="flex-column">
+      <b-nav-item>
+        <b-img src="/icons/house-door-fill.svg"></b-img>
+      </b-nav-item>
+      <b-nav-item>
+        <b-img src="/icons/layers-half.svg"></b-img>
+      </b-nav-item>
+      <b-nav-item>
+        <b-img src="/icons/calculator-fill.svg"></b-img>
+      </b-nav-item>
+      <b-nav-item>
+        <b-img src="/icons/calendar2-date-fill.svg"></b-img>
+      </b-nav-item>
+      <b-nav-item>
+        <b-img src="/icons/info-circle-fill.svg"></b-img>
+      </b-nav-item>
+    </b-navbar-nav>
+
+    <b-navbar-nav class="flex-column">
+      <b-nav-item>
+        <b-img src="/icons/bell-fill.svg"></b-img>
+      </b-nav-item>
+      <b-nav-item>
+        <b-avatar src="/images/homeowner.png"></b-avatar>
+      </b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>

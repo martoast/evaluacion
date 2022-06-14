c59ef7129ab9044eada2f92772007e6210f55738
Teresa Wu Feng <twufeng@gomultitaskr.com>
2022-06-13 17:03:06 -0700

    fixing monthly and equity card style

--- components/cards/EquityGain.vue
+++ components/cards/EquityGain.vue
@@ -1,41 +1,34 @@
 <template>
-  <div>
-    <div>
-      <b-card class="border-05 border-0 shadow mt-4" no-body>
-        <b-card-body class="p-4">
-          <div class="position-relative">
-            <!-- <b-img
-              src="/images/overlay.png"
-              fluid-grow
-              style="top: 0; left: 0; position: absolute; z-index: -1"
-            ></b-img> -->
-          </div>
-          <b-card-sub-title
-            sub-title-text-variant="primary"
-            class="font-weight-bold mb-3"
-          >
-            <b-img src="/icons/monthly-payment.svg"></b-img> Home Equity Gain
-          </b-card-sub-title>
+  <b-card
+    class="border-radius-05 border-0 custom-card-shadow h-auto"
+    style="margin-right: 24px"
+    no-body
+  >
+    <b-card-body class="d-flex flex-column" style="padding: 20px; flex: 1 1 0">
+      <b-card-sub-title
+        sub-title-text-variant="primary"
+        class="font-weight-600 font-size-14"
+        style="margin-bottom: 24px"
+      >
+        <b-img src="/icons/home-equity.svg"></b-img> Home Equity Gain
+      </b-card-sub-title>
 
-          <b-card-text class="text-gray mt-3 mb-4">
-            Add your Property Value to discover your Home Equity Gain.
-          </b-card-text>
+      <div class="h-100 d-flex flex-column justify-content-between">
+        <b-card-text class="text-muted font-size-14">
+          Add your Property Value to discover your Home Equity Gain.
+        </b-card-text>
 
-          <b-button
-            style="
-              border-color: rgba(181, 145, 249, 1);
-              background: transparent;
-            "
-            border-variant="secondary"
-            class="font-weight-600 text-primary"
-            @click="onShowOverlay()"
-          >
-            Add Property Value
-          </b-button>
-        </b-card-body>
-      </b-card>
-    </div>
-  </div>
+        <b-button
+          style="border-color: rgba(181, 145, 249, 1); background: transparent"
+          border-variant="secondary"
+          class="font-weight-600 text-primary"
+          @click="onShowOverlay()"
+        >
+          Add Property Value
+        </b-button>
+      </div>
+    </b-card-body>
+  </b-card>
 </template>
 
 <script>
@@ -55,3 +48,9 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.btn {
+  padding: 8px 12px;
+}
+</style>
--- pages/calculator/kitchen.vue
+++ pages/calculator/kitchen.vue
@@ -1,7 +1,140 @@
-<template></template>
+<template>
+  <div>
+    <div class="main-container">
+      <b-navbar class="main-navbar">
+        <div class="nav-logo">
+          <b-avatar src="/images/multitaskr-avatar.png"></b-avatar>
+        </div>
+
+        <b-navbar-nav class="flex-column">
+          <b-nav-item>
+            <b-img src="/kitchen-icons/house-door-fill.svg"></b-img>
+          </b-nav-item>
+          <b-nav-item>
+            <b-img src="/kitchen-icons/layers-half.svg"></b-img>
+          </b-nav-item>
+          <b-nav-item>
+            <b-img src="/kitchen-icons/calculator-fill.svg"></b-img>
+          </b-nav-item>
+          <b-nav-item>
+            <b-img src="/kitchen-icons/calendar2-date-fill.svg"></b-img>
+          </b-nav-item>
+          <b-nav-item>
+            <b-img src="/kitchen-icons/info-circle-fill.svg"></b-img>
+          </b-nav-item>
+        </b-navbar-nav>
+
+        <b-navbar-nav class="flex-column">
+          <b-nav-item>
+            <b-img src="/kitchen-icons/bell-fill.svg"></b-img>
+          </b-nav-item>
+          <b-nav-item>
+            <b-avatar src="/images/homeowner.png"></b-avatar>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-navbar>
+
+      <div class="content-container">
+        <b-img
+          src="/images/bg-kitchen.png"
+          class="vh-100 position-absolute"
+          fluid-grow
+        ></b-img>
+
+        <div class="calculator-card-container">
+          <div class="bg-mask"></div>
+          <div class="calculator-card-group">
+            <monthly-payment></monthly-payment>
+
+            <equity-gain></equity-gain>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
-export default {};
+import MonthlyPayment from "~/components/cards/MonthlyPayment.vue";
+import EquityGain from "~/components/cards/EquityGain.vue";
+import PriceRange from "~/components/cards/PriceRange.vue";
+
+export default {
+  components: {
+    MonthlyPayment,
+    EquityGain,
+    PriceRange,
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.custom-card-shadow {
+  box-shadow: 0px 3.20329px 12.8132px rgba(0, 0, 0, 0.15);
+}
+
+.border-radius-05 {
+  border-radius: 0.5rem;
+}
+
+.main-container {
+  display: flex;
+}
+
+.content-container {
+  position: relative;
+  width: 100%;
+  display: flex;
+  flex: 1 1 0;
+  flex-direction: column;
+}
+
+.main-navbar {
+  position: relative;
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100vh;
+  padding: 0;
+  border: 1px solid rgba(#ccd2dd, 0.16);
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 0.6) 0%,
+    rgba(255, 255, 255, 0.1) 100.24%
+  );
+  backdrop-filter: blur(42px);
+
+  .nav-logo {
+    padding: 17px 12px;
+  }
+
+  .nav-item {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 64px;
+    height: 64px;
+  }
+}
+
+.calculator-card-container {
+  position: relative;
+  margin-top: auto;
+
+  .bg-mask {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(0deg, #ffffff, #ffffff),
+      linear-gradient(180deg, #ffffff 0%, rgba(255, 255, 255, 0.1) 100.24%);
+    mix-blend-mode: lighten;
+    opacity: 0.9;
+    backdrop-filter: blur(190.076px);
+  }
+
+  .calculator-card-group {
+    display: flex;
+    padding: 24px;
+  }
+}
+</style>
